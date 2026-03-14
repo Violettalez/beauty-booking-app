da49a6f747ba8caff20b93a37f6be5602d927cb2
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2026-03-14 11:57:35 +0200

    main content home page

---
+++ js/swiper.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper(".reviews-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
